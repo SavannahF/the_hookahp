02b5d475de9c864261ab39d90df43477b7726872
nadiadorado@gmail.com <ndorado@uncc.edu>
2021-05-25 19:08:06 -0400

    I changed the controllers, models & seeds

--- server.js
+++ server.js
@@ -1,23 +1,41 @@
-const express = require('express')
-const app = express()
-const exphbs = require('express-handlebars')
+// const path = require("path");
+const express = require("express");
+// const exphbs = require("express-handlebars");
+const routes = require("./controllers");
 
-app.engine('handlebars', exphbs())
-app.set('view engine', 'handlebars')
+const sequelize = require("./config/connection");
 
+const app = express();
+const PORT = process.env.PORT || 3006;
 
+// const hbs = exphbs.create({ helpers });
 
-app.get('/', (req,res) => {
-   res.render('home')
-})
+// app.engine("handlebars", exphbs());
+// app.set("view engine", "handlebars");
 
-app.get('/hookahs', (req, res) => {
-    const hookahs = 'search the database for hookahs'
-    const prices = [5.00, 49.99,100]
-    res.render('hookahs', {
-        prices,
-        hookahs
-    })
-})
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// app.use(express.static(path.join(__dirname, "public")));
 
-app.listen(3000)
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}!`);
+  });
+});
+
+// app.get('/', (req,res) => {
+//    res.render('home')
+// })
+
+// app.get('/hookahs', (req, res) => {
+//     const hookahs = 'search the database for hookahs'
+//     const prices = [5.00, 49.99,100]
+//     res.render('hookahs', {
+//         prices,
+//         hookahs
+//     })
+// })
+
+// app.listen(3000)
